def43860943ecdd574cbc810b01eedd2829ec5da
kimu805 <kimukimuok805@gmail.com>
2025-10-11 15:19:39 +0900

    commit

--- src/index.js
+++ src/index.js
@@ -3,34 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import StateParent from './components/StateParent';
-import EventObj from './components/EventObj';
-import EventOnce from './components/EventOnce';
-import StateForm from './components/StateForm';
-import StateFormUC from './components/StateFormUC';
-import FormTextarea from './components/FormTextarea';
-import FormSelect from './components/FormSelect';
-import FormList from './components/FormList';
-import StateTodo from './components/StateTodo';
-import StateForm2 from './components/StateForm2';
-import FormBasic from './components/FormBasic';
-import FormYup from './components/FormYup';
-import LazyBasic from './components/LazyBasic';
-import LazyMulti from './components/LazyMulti';
-import SuspenseSimple from './components/SuspenseSimple';
-import SuspenseResult from './components/SuspenseReslut';
-import PortalBasic from './components/PortalBasic';
-import ErrorRoot from './components/ErrorRoot';
-import ErrorRetryRoot from './components/ErrorRetryRoot';
-import MaterialBasic from './components/MaterialBasic';
-import MaterialDrawer from './components/MaterialDrawer';
-import MaterialGrid from './components/MaterialGrid';
+import MaterialMode from './components/MaterialMode';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div id="dialog"></div>
-    <MaterialGrid />
+    <MaterialMode />
   </React.StrictMode>
 );
 
